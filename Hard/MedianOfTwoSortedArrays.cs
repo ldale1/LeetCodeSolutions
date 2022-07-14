b22922eb782580ea47e41e97e8e4ebd87860035e
ldale <liamdale15@gmail.com>
2022-07-14 12:42:30 +1000

    MedianOfTwoSortedArrays

---
+++ Hard/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,70 @@
+﻿//Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+//The overall run time complexity should be O(log (m+n)).
+
+//nums1.length == m
+//nums2.length == n
+//0 <= m <= 1000
+//0 <= n <= 1000
+//1 <= m + n <= 2000
+//- 106 <= nums1[i], nums2[i] <= 106
+
+using NUnit.Framework;
+
+var tests = new[]
+{
+    (
+        nums1: new [] {1, 3},
+        nums2: new [] {2},
+        median: 2.0
+    ),
+    (
+        nums1: new [] {1, 2},
+        nums2: new [] {3, 4},
+        median: 2.5
+    ),
+};
+
+foreach (var test in tests)
+{
+    Assert.That(FindMedianSortedArrays(test.nums1, test.nums2), Is.EqualTo(test.median));
+}
+
+// Solution
+double FindMedianSortedArrays(int[] nums1, int[] nums2)
+{
+    // Move along the two arrays in asending order, tracking the current and previous.
+    // When x iterations are hit these will decide the median.
+    // Doesn't quite hit the log(m+n) requirement probably needs binary search somewhere
+    var previous = 0.0;
+    var current = 0.0;
+
+    // head indices
+    var head1 = 0;
+    var head2 = 0;
+
+    var total = (nums1.Length + nums2.Length);
+    var iterations = total / 2 + 1;
+    for (var i = 0; i < iterations; i++)
+    {
+        var num1 = head1 < nums1.Length 
+            ? nums1[head1] 
+            : double.MaxValue;
+        var num2 = head2 < nums2.Length 
+            ? nums2[head2] 
+            : double.MaxValue;
+
+        // Find which is array is greater and move along
+        var temp = current;
+        current = num1 < num2
+            ? nums1[head1++]
+            : nums2[head2++];
+
+        previous = temp;
+    }
+
+    // If there are an even amount of elements, find the average of previous and current
+    return total % 2 == 0
+        ? (previous + current) / 2.0
+        : current;
+}
---
+++ Medium/PeekingIterator.cs
@@ -0,0 +1,62 @@
+﻿//Design an iterator that supports the peek operation on an existing iterator in addition to the hasNext and the next operations.
+
+//Implement the PeekingIterator class:
+//PeekingIterator(Iterator<int> nums) Initializes the object with the given integer iterator iterator.
+//int next() Returns the next element in the array and moves the pointer to the next element.
+//boolean hasNext() Returns true if there are still elements in the array.
+//int peek() Returns the next element in the array without moving the pointer.
+//using NUnit.Framework;
+
+using NUnit.Framework;
+
+IEnumerable<object> RunIterator(PeekingIterator p, params Func<PeekingIterator, object>[] methods)
+{
+    foreach (var method in methods)
+    {
+        yield return method(p);
+    }
+}
+
+var tests = new (IEnumerator<int> Input, Func<PeekingIterator, object[]> Sequence, object[] Expected)[]
+{
+    (
+        Input: new List<int>{ 1, 2, 3 }.GetEnumerator(),
+        Sequence: p => RunIterator(p, p => p.Next(), p => p.Peek(), p => p.Next(), p => p.Next(), p => p.HasNext()).ToArray(),
+        Expected:  new object[] { 1, 2, 2, 3, false }
+    ),
+};
+
+foreach (var test in tests)
+{
+    // iterators refers to the first element of the array.
+    test.Input.MoveNext();
+
+    var actual = test.Sequence(new PeekingIterator(test.Input));
+    Assert.That(actual, Is.EquivalentTo(test.Expected));
+}
+
+class PeekingIterator
+{
+    private Queue<int> m_queue = new Queue<int>();
+
+    // iterators refers to the first element of the array.
+    public PeekingIterator(IEnumerator<int> iterator)
+    {
+        do
+        {
+            m_queue.Enqueue(iterator.Current);
+        } while (iterator.MoveNext());
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public int Peek()
+        => m_queue.Peek();
+
+    // Returns the next element in the iteration and advances the iterator.
+    public int Next()
+        => m_queue.Dequeue();
+
+    // Returns false if the iterator is refering to the end of the array of true otherwise.
+    public bool HasNext()
+        => m_queue.Any();
+}
